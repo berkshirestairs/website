e0794123ef224298b659ed9e05176f6432a906d2
tobiktrueman <167309712+tobiktrueman@users.noreply.github.com>
2024-06-02 23:10:32 +0100

    asd

---
+++ my-gallery/src/App.js
@@ -0,0 +1,80 @@
+
+import React, { useState, useCallback } from "react";
+import logo from './logo.svg';
+import './App.css';
+import Gallery from "react-photo-gallery"
+import { photos } from './photos.tsx';
+import Carousel, { Modal, ModalGateway } from "react-images";
+
+
+
+
+function App() {
+
+
+
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+  
+
+
+
+
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+
+        
+          
+         <Gallery photos={photos} on onClick={openLightbox} />
+          
+       
+         <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+      
+
+
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-gallery/src/photos.tsx
@@ -0,0 +1,110 @@
+
+export const photos = [
+    {
+      src: 'https://www.sobczak.com.pl/img/realizacje/samonosne/25s/thumbnails/IMG_8687.jpg',
+      width: 4,
+      height: 3
+    },
+    {
+      src: 'https://www.sobczak.com.pl/img/realizacje/samonosne/25s/thumbnails/IMG_8706.jpg',
+      width: 1,
+      height: 1
+    },
+
+    {
+      src: 'https://www.sobczak.com.pl/img/realizacje/samonosne/25s/thumbnails/IMG_8687.jpg',
+      width: 4,
+      height: 3
+    },
+    {
+      src: 'https://www.sobczak.com.pl/img/realizacje/samonosne/25s/thumbnails/IMG_8706.jpg',
+      width: 1,
+      height: 1
+    },
+
+
+    {
+      src: 'https://www.sobczak.com.pl/img/realizacje/samonosne/25s/thumbnails/IMG_8687.jpg',
+      width: 4,
+      height: 3
+    },
+    {
+      src: 'https://www.sobczak.com.pl/img/realizacje/samonosne/25s/thumbnails/IMG_8706.jpg',
+      width: 1,
+      height: 1
+    },
+
+
+    {
+      src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+      width: 1,
+      height: 1
+    },
+    {
+      src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+      width: 4,
+      height: 3
+    },
+
+
+    {
+      src: 'https://www.sobczak.com.pl/img/realizacje/samonosne/25s/thumbnails/IMG_8687.jpg',
+      width: 4,
+      height: 3
+    },
+    {
+      src: 'https://www.sobczak.com.pl/img/realizacje/samonosne/25s/thumbnails/IMG_8706.jpg',
+      width: 2,
+      height: 1
+    },
+
+    {
+      src: 'https://www.sobczak.com.pl/img/realizacje/samonosne/25s/thumbnails/IMG_8687.jpg',
+      width: 4,
+      height: 3
+    },
+    {
+      src: 'https://www.sobczak.com.pl/img/realizacje/samonosne/25s/thumbnails/IMG_8706.jpg',
+      width: 3,
+      height: 1
+    }
+
+
+
+  ];
+
